7786577728695f88f07fd0dc2760976ffe8c1ffc
SELENA FLANNERY <SELENA.FLANNERY@GMAIL.COM>
2016-03-09 09:12:13 -0800

    fixed bugs with closing socket

--- src/server.py
+++ src/server.py
@@ -3,34 +3,42 @@
 import socket
 
 
+class Response(object):
+    """Create Response Class."""
+
+    def __init__(self, code, headers=None):
+        """Init Response with Status code."""
+        self.protocol = "HTTP/1.1 "
+        self.code = code
+        self.status = [self.protocol, code + "\n"]
+        self.headers = headers
+        self.body = "This is some text -- body text"
+
+    def return_response_string(self):
+        """Return this Response Instances's response string."""
+        blank_line = "<CRLF>\n"
+
+        response_list = self.status
+        for k, v in self.headers.items():
+            response_list.append(k + ": ")
+            response_list.append(v + "\n")
+        response_list.append(blank_line)
+        response_list.append(self.body)
+
+        response_string = "{}" * len(response_list)
+
+        return response_string.format(*response_list)
+
+
 def response_ok():
-    status = {
-        "protocol": "HTTP/1.1 ",
-        "code": "200 OK"
-    }
-
-    headers = {
-        "Content-Type": "text/plain"
-    }
-
-    blank_line = "<CRLF>\n"
-
-    body = "Some silly text"
-
-    response_list = [status["protocol"], status["code"] + "\n"]
-    for k, v in headers.items():
-        response_list.append(k + ": ")
-        response_list.append(v + "\n")
-    response_list.append(blank_line)
-    response_list.append(body)
-
-    response_string = "{}" * len(response_list)
-
-    return response_string.format(*response_list)
+    headers = {"Content-Type": "text/plain"}
+    response = Response("200 OK", headers=headers)
+    return response.return_response_string()
 
 
 def response_error():
-    pass
+    response = Response("500 INTERNAL SERVER ERROR")
+    return response.return_response_string()
 
 
 def make_socket():
@@ -38,29 +46,23 @@ def make_socket():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
     address = ('127.0.0.1', 5000)
     server.bind(address)
-    return server
-
-
-def socket_listen(server):
-    """Listen for a client and accept connection."""
     server.listen(1)
-    conn, addr = server.accept()
-    return conn, addr
+    return server
 
 
 def server_read(conn):
     """Read incoming client message and create echo message to client."""
     buffer_length = 16
     message_complete = False
-    echo_message = ""
+    message = ""
 
     while not message_complete:
         part = conn.recv(buffer_length)
-        echo_message += part.decode('utf8')
+        message += part.decode('utf8')
         if len(part) < buffer_length:
             break
 
-    return echo_message
+    return message
 
 
 def server():
@@ -69,16 +71,20 @@ def server():
         this_server = make_socket()
         print('socket open')
         while True:
-            conn, addr = socket_listen(this_server)
-            echo_message = server_read(conn)
-            print(echo_message)
+            conn, addr = this_server.accept()
+            message = server_read(conn)
+            print(message)
             response = response_ok()
             print(response)
             conn.sendall(response.encode('utf-8'))
             conn.close()
 
     except KeyboardInterrupt:
-        print('connection closing')
-        conn.close()
+        try:
+            conn.close()
+        except NameError:
+            pass
+
+    finally:
         print('Socket closing')
         this_server.close()
